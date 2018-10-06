a9a84f2c2573653aa80c4b7a20718efb660e7666
Mikael Vandmo <mikael.vandmo@wirelesscar.com>
2018-10-06 20:22:19 +0200

    Initial

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('VERSION') as version_file:
+    version = version_file.read().strip()
+
+setup(
+    name='plit',
+    version=version,
+    url='https://github.com/vandmo/plit',
+    license='Apache License 2.0',
+    author='vandmo',
+    description='Clines sets of git repositories',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'pyyaml'
+    ],
+    entry_points='''
+        [console_scripts]
+        plit=plit.cli:cli
+    ''',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha'
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
