aa7971e1a6e6f960886d83c897985aa73f0596b3
Mikael Vandmo <mikael.vandmo@wirelesscar.com>
2018-10-07 10:39:54 +0200

    Author email

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@ from setuptools import setup, find_packages
 with open('VERSION') as version_file:
     version = version_file.read().strip()
 
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
 setup(
     name='plit',
     version=version,
     url='https://github.com/vandmo/plit',
     license='Apache License 2.0',
     author='vandmo',
+    author_email='mikael@vandmo.se',
     description='Clones sets of git repositories',
+    long_description=readme,
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
